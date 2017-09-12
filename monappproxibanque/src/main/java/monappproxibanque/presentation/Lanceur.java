d832e10df57f37fcf309c4d088af7668364062cf
'Clemy507' <'cpierre507@gmail.com'>
2017-09-12 15:10:56 +0200

    ajout lanceur et presentationswing 2

--- monappproxibanque/src/main/java/monappproxibanque/presentation/Lanceur.java
+++ monappproxibanque/src/main/java/monappproxibanque/presentation/Lanceur.java
@@ -8,6 +8,7 @@ public class Lanceur {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//creation de la fenetre swing
 		PresentationSwing f = new PresentationSwing() ;
 		
 		f.setVisible(true);
